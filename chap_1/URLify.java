222e398cb9a6f2aef5c9c94c31ffd54c127ab3a7
dragonfly9113 <weilin.william.luo@gmail.com>
2018-04-12 23:10:33 -0700

    Create URLify.java

---
+++ chap_1/URLify.java
@@ -0,0 +1,50 @@
+//'main' method must be in a class 'Rextester'.
+//Compiler version 1.8.0_111
+
+import java.util.*;
+import java.lang.*;
+
+// Chapter 1
+// 1.3 URLify
+
+class Rextester
+{  
+    public static void main(String args[])
+    {
+        String str = "A Good Day!    ";
+        
+        System.out.println(URLify_1(str, str.length()-4));
+    }
+    
+    // String concatenation is implemented through the StringBuilder(or StringBuffer) class and its append method.
+    // This method needs an extra String
+    static String URLify_1(String s, int actualLen)
+    {
+        String t = "";
+        
+        if (s.length() == 0 || actualLen == 0) return t;
+        
+        for (int i = 0; i < actualLen; i++)
+        {
+            char c = s.charAt(i);
+            
+            if (c == ' ')
+                t = t + "%20";
+            else
+                t = t + c;
+        }
+            
+        return t;
+    }
+    
+    
+    // If implementing in Java, please use a character array so that you can perform this operation in place
+    static String URLify_2(String s, int actualLen)
+    {
+        return "";
+     
+        
+        
+        
+    }
+}
