9780ef0de175a46e6ba167cd47047845dbe975d1
dragonfly9113 <weilin.william.luo@gmail.com>
2018-04-14 05:51:07 +0800

    Update URLify.java

--- chap_1/URLify.java
+++ chap_1/URLify.java
@@ -12,8 +12,13 @@ class Rextester
     public static void main(String args[])
     {
         String str = "A Good Day!    ";
+        char[] arr = str.toCharArray();
         
-        System.out.println(URLify_1(str, str.length()-4));
+        //System.out.println(URLify_1(str, str.length()-4));
+        System.out.println("arr.length = " + arr.length);
+        System.out.println(arr);
+        
+        System.out.println(URLify_2(arr, arr.length - 4));
     }
     
     // String concatenation is implemented through the StringBuilder(or StringBuffer) class and its append method.
@@ -41,13 +46,40 @@ class Rextester
     // If implementing in Java, please use a character array so that you can perform this operation in place
     //
     // s.toCharArray(): a newly allocated character array whose length is the length of this string and whose contents are initialized to contain the character sequence represented by this string.
-    static String URLify_2(String s, int actualLen)
+    static char[] URLify_2(char[] s, int len)
     {
-        char[] arr = s.toCharArray();
+        if (s.length == 0 || len == 0) return s;
         
+        // count number of spaces in s
+        int count = 0;
+        for (int i = 0; i < len; i++)
+            if (s[i] == ' ') count++;
         
+        int newLen = len + 2 * count + 1;
+        char[] new_s = new char[newLen];
+        System.arraycopy( s, 0, new_s, 0, len );
         
-        return "";
-     
+        System.out.println("new_s.length = " + new_s.length);
+        System.out.println(new_s);
+        
+        int index = newLen - 2;
+        for (int i = len - 1; i >= 0; i--)
+        {
+            if (new_s[i] != ' ')
+            {
+                new_s[index] = new_s[i];
+                index--;
+            }
+            else
+            {
+                new_s[index] = '0';
+                new_s[index - 1] = '2';
+                new_s[index - 2] = '%';
+                index = index - 3;
+            }
+        }
+
+        new_s[newLen - 1] = '\0';
+        return new_s;
     }
 }
