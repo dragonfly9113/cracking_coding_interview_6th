2ed0ef1143bff50deca862afbf9400bb16a0dfb5
dragonfly9113 <weilin.william.luo@gmail.com>
2018-04-14 08:19:10 +0800

    Update isPermutationOfPalindrome.java

--- chap_1/isPermutationOfPalindrome.java
+++ chap_1/isPermutationOfPalindrome.java
@@ -17,9 +17,17 @@ class Rextester
 {  
     public static void main(String args[])
     {
-        String s = "Tact Coa";
+        String s1 = "Tact Coa";  // true
+        System.out.println("s1 = " + isPermutationOfPalindrome_1(s1));
+
+        String s2 = "Tact CoaA";  // false
+        System.out.println("s2 = " + isPermutationOfPalindrome_1(s2));
+
+        String s3 = "Tact Coa O";  // true
+        System.out.println("s3 = " + isPermutationOfPalindrome_1(s3));
         
-        System.out.println("isPermutationOfPalindrome = " + isPermutationOfPalindrome(s));
+        String s4 = "Tact Coa coe";  // false
+        System.out.println("s4 = " + isPermutationOfPalindrome_1(s4));
     }
     
     // Assume case insensitive, space is not significant, ASCII
@@ -54,4 +62,35 @@ class Rextester
         
         return true;
     }
+    
+    // Use a bit vector to reduce storage space
+    // Assume letters only from a to z, thus one integer (32-bit) is good enough
+    static boolean isPermutationOfPalindrome_1(String str)
+    {
+        int count_int = 0; // assume letters only from a - z
+        
+        // convert all letters to lower case since we assume case insensitive
+        String s = str.toLowerCase();
+        
+        // count each letter's occurrance number and save in a table
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s.charAt(i) == ' ') continue; // ignore space (don't count)
+            
+            int val = s.charAt(i) - 'a';
+            count_int ^= (1 << val); // exclusive OR (^)
+        }
+        
+        // check the count_int: we can only have 0 or 1 bit set to one
+        int oddOccurrance = 0;
+        for (int i = 0; i < 32; i++)
+        {
+            if (((count_int >> i) & 1) == 1) oddOccurrance++;
+            
+            if (oddOccurrance > 1) return false;
+        }
+        
+        return true;
+    }
+
 }
