d0eac00f4812ccc6b22085ec6dcf164a42c1b188
dragonfly9113 <weilin.william.luo@gmail.com>
2018-04-17 22:51:35 -0700

    Update strCompress.java

--- chap_1/strCompress.java
+++ chap_1/strCompress.java
@@ -18,8 +18,8 @@ class Rextester
         String s = "aabcccccaaa";
         String s1 = "abcd";
         
-        System.out.println("strCompress(s) = " + strCompress(s));
-        System.out.println("strCompress(s1) = " + strCompress(s1));
+        System.out.println("strCompress(s) = " + strCompress_1(s));
+        System.out.println("strCompress(s1) = " + strCompress_1(s1));
     }
 
     static String strCompress(String s)
@@ -49,4 +49,35 @@ class Rextester
         
         return (t.length() < s.length()) ? t : s;
     }
+    
+    // String concatenation in Java is of time complexity of O(n^2) because of the fact that a String object
+    // is immutable and thus many intermediate String objects need to be created to hold intermediate results.
+    // Therefore, it is more efficient to use StringBuilder or StringBuffer, which is of time complexity of O(n).
+    static String strCompress_1(String s)
+    {
+        StringBuilder t = new StringBuilder();
+        
+        int count = 0;
+        char preC = '\0';
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (c != preC) {
+                if (preC != '\0')
+                    t.append(count);
+                t.append(c);
+                count = 1;
+                preC = c;
+                continue;
+            }
+            
+            count++;
+        }
+        
+        t.append(count);
+        
+        return (t.length() < s.length()) ? t.toString() : s;
+    }
+
 }
