b6ae670b928c1a0a6c2b1a691414418ede18d66e
dragonfly9113 <weilin.william.luo@gmail.com>
2018-04-22 16:12:24 -0700

    Update zeroMatrix.java

--- chap_1/zeroMatrix.java
+++ chap_1/zeroMatrix.java
@@ -4,8 +4,6 @@
 import java.util.*;
 import java.lang.*;
 
-// 1.8: Zero Matrix
-
 class Rextester
 {  
     public static void main(String args[])
@@ -19,7 +17,7 @@ class Rextester
             System.out.println();
         }
         
-        if (zeroMatrix(matrix) < 0) return;
+        if (zeroMatrix_1(matrix) < 0) return;
         
         System.out.println();
         for (int i = 0; i < matrix.length; i++) {
@@ -56,6 +54,42 @@ class Rextester
         for (int j = 0; j < N; j++)
             if (column[j] == 1)
                 for (int i = 0; i < M; i++) m[i][j] = 0;
+            
+        return 0;
+    }
+    
+    // Try to save info in the matrix itself, thus save some space
+    static int zeroMatrix_1(int[][] m)
+    {
+        if (m.length == 0 || m[0].length == 0) return -1;
+        
+        int M = m.length, N = m[0].length;
+        
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (m[i][j] == 0) {
+                    m[i][0] = 0;    // set the first element (0th column) in ith row to zero as indicator 
+                    m[0][j] = 0;    // set the first element (0th row) in jth column to zero as indicator
+                }
+            }
+        }
+
+        // check the in-matrix indicators (first row and first column) and set zeros accordingly
+        boolean firstRowZero = false;
+        for (int i = 0; i < M; i++) 
+            if (m[i][0] == 0) {
+                if (i == 0) { firstRowZero = true; continue; }
+                
+                for (int j = 0; j < N; j++) m[i][j] = 0;
+            }
+               
+        for (int j = 0; j < N; j++)
+            if (m[0][j] == 0)
+                for (int i = 0; i < M; i++) m[i][j] = 0;        
+
+        // delay the clearing of the first row to here
+        if (firstRowZero)
+            for (int j = 0; j < N; j++) m[0][j] = 0;
             
         return 0;
     }
