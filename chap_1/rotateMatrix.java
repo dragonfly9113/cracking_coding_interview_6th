389e28796afda849e36df91788a21ba63da6b302
dragonfly9113 <weilin.william.luo@gmail.com>
2018-04-22 16:11:17 -0700

    Update rotateMatrix.java

--- chap_1/rotateMatrix.java
+++ chap_1/rotateMatrix.java
@@ -4,90 +4,94 @@
 import java.util.*;
 import java.lang.*;
 
-// 1.7 Rotate Matrix
-
 class Rextester
 {  
     public static void main(String args[])
     {
-        int N = 4, count = 0;
-        int[][] m = new int[N][N];
+        int[][] matrix = {{1,2,0,4},{0,3,4,5},{6,7,8,9}};
         
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < N; j++)
-                m[i][j] = count++;
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++)
-                System.out.print(m[i][j] + " ");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++)
+                System.out.print(matrix[i][j] + " ");
+            
             System.out.println();
         }
         
-        rotateMatrix(m, N);
-
-        System.out.println();
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++)
-                System.out.print(m[i][j] + " ");
-            System.out.println();
-        }
+        if (zeroMatrix_1(matrix) < 0) return;
         
-        rotateMatrix(m);
-
         System.out.println();
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++)
-                System.out.print(m[i][j] + " ");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++)
+                System.out.print(matrix[i][j] + " ");
+            
             System.out.println();
         }
     }
     
-    static void rotateMatrix(int[][] matrix, int N)
+    // Time complexity: O(M * N)
+    // Space complexity: O(M + N)
+    static int zeroMatrix(int[][] m)
     {
-        int tmp = 0;
-        int l = 0; // layer number
-        int n = N;
+        if (m.length == 0 || m[0].length == 0) return -1;
         
-        for (int n1 = N; n1 >= 2; n1 = n1 - 2) {
-            for (int i = 0; i < n1 - 1; i++) {
-                tmp = matrix[l][i + l];
-                
-                matrix[l][i + l] = matrix[n - i - 1 - l][l];
-                
-                matrix[n - i - 1 - l][l] = matrix[n - 1 -l][n - i - 1 -l];
-                
-                matrix[n - 1 -l][n - i - 1 -l] = matrix[i + l][n - 1 - l];
-                
-                matrix[i + l][n - 1 - l] = tmp;
-            }
-            l++;
-        }
-    }
-
-    // another version with easier-to-understand way
-    static void rotateMatrix(int[][] m)
-    {
-        if (m.length == 0) return;
+        int M = m.length, N = m[0].length;
         
-        if (m.length != m[0].length) return;
-
-        for (int layer = 0; layer < m.length/2; layer++) {
-
-            int first = layer;
-            int last = m.length - layer - 1;
+        int[] row = new int[M];
+        int[] column = new int[N];
         
-            for (int i = first; i < last; i++) {
-                int offset = i - first;
-                int tmp = m[first][i];
-                
-                m[first][i] = m[last - offset][first];
-                
-                m[last - offset][first] = m[last][last - offset];
-                
-                m[last][last - offset] = m[i][last];
-                
-                m[i][last] = tmp;
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (m[i][j] == 0) {
+                    row[i] = 1; column[j] = 1;
+                }
             }
         }
+
+        for (int i = 0; i < M; i++) 
+            if (row[i] == 1)
+                for (int j = 0; j < N; j++) m[i][j] = 0;
+        
+        for (int j = 0; j < N; j++)
+            if (column[j] == 1)
+                for (int i = 0; i < M; i++) m[i][j] = 0;
+            
+        return 0;
     }
+    
+    // Try to save info in the matrix itself, thus save some space
+    static int zeroMatrix_1(int[][] m)
+    {
+        if (m.length == 0 || m[0].length == 0) return -1;
+        
+        int M = m.length, N = m[0].length;
+        
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (m[i][j] == 0) {
+                    m[i][0] = 0;    // set the first element (0th column) in ith row to zero as indicator 
+                    m[0][j] = 0;    // set the first element (0th row) in jth column to zero as indicator
+                }
+            }
+        }
+
+        // check the in-matrix indicators (first row and first column) and set zeros accordingly
+        boolean firstRowZero = false;
+        for (int i = 0; i < M; i++) 
+            if (m[i][0] == 0) {
+                if (i == 0) { firstRowZero = true; continue; }
+                
+                for (int j = 0; j < N; j++) m[i][j] = 0;
+            }
+               
+        for (int j = 0; j < N; j++)
+            if (m[0][j] == 0)
+                for (int i = 0; i < M; i++) m[i][j] = 0;        
+
+        // delay the clearing of the first row to here
+        if (firstRowZero)
+            for (int j = 0; j < N; j++) m[0][j] = 0;
+            
+        return 0;
+    }
+    
 }
